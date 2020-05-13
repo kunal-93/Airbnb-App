0f5d3c880750281b6895433214e9c6bf645dd46b
kunal <knnauls@gmail.com>
2020-05-13 14:04:17 -0400

    fixed a bug that allowed checkout and checkin date to be same, closes #9

--- controller/functionality/general.js
+++ controller/functionality/general.js
@@ -14,5 +14,37 @@ const stringToHTML = str => {
 
 };
 
+const getLocalTodayDate = () => {
+    const currDateObj = new Date();
+
+    let [currMonth, currDay, currYear] = currDateObj.toLocaleDateString().split('/');
+
+    currMonth = currMonth>9?currMonth:'0'+currMonth;
+
+    currDay = currDay>9?currDay:'0'+ currDay;
+        
+    return   currYear + "-" + currMonth + "-" + currDay;
+}
+
+const validateBookingDates = (from, to) => {
+
+    const currDate = getLocalTodayDate();
+
+    let result = {}
+    if(from < currDate || to < currDate){
+        result.msg = "Dates cannot be in past";
+        result.errorFound = true;
+    }
+    else if(to <= from){
+        result.msg = "Checkout must be atleast a day after checkin";
+        result.errorFound = true;
+    }
+
+    return result;
+    
+}
+
 module.exports.stringToHTML = stringToHTML;
 module.exports.trimFields = trimFields;
+module.exports.validateBookingDates = validateBookingDates;
+module.exports.getLocalTodayDate = getLocalTodayDate;
